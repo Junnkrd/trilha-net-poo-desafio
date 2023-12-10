bd7d52f09a967421bf6b378e09f93b8ea43ecacd
Junnkrd <jevertoncosta60@gmail.com>
2023-12-10 18:56:03 -0300

    udpate

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,24 +1,31 @@
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
+
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        protected string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string iMEI, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = iMEI;
+            Memoria = memoria;
+
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando com {Modelo}...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"A partir {Modelo} Recebendo ligação...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("4199999", "XS", "111111", 11);
+
+Nokia nokia = new Nokia("4188888", "TIJOLAO", "4148464681", 12);
+
+iphone.InstalarAplicativo("Pokemon GO");
+nokia.InstalarAplicativo("Strava");
+
+
+iphone.Ligar();
+
+nokia.ReceberLigacao();
+
+
+
